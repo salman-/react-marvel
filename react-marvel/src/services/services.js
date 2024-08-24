f7da9eb01bb296a9badc742fd4aafe0d4887e44b
salmanlashkarara <salmanlashkarara@gmail.com>
2024-08-24 10:13:23 +0200

    refactor services

--- react-marvel/src/services/services.js
+++ react-marvel/src/services/services.js
@@ -2,11 +2,6 @@ import environment from '../environment/environment.js';
 import * as CryptoJS from 'crypto-js';
 
 
-
-export function doesNotContainSubstring(mainString, substring) {
-    return mainString.indexOf(substring) === -1;
-}
-
 export function generateHash(publicKey, privateKey, timeStamp) {
 
     let input = `${timeStamp}${privateKey.trim()}${publicKey.trim()}`;
@@ -42,33 +37,3 @@ export const buildAuthenticationParameters = () => {
     return buildRequestParameters(timeStamp, hash, publicKey);
 }
 
-export function randomlySelectMarvels(marvelWithThumbnail) {
-    let randomlySelectedMarvel = [];
-    for (let i = 0; i < 6; i++) {
-        let index = Math.floor(Math.random() * marvelWithThumbnail.length);
-        randomlySelectedMarvel.push(marvelWithThumbnail[index]);
-    }
-    return randomlySelectedMarvel;
-}
-
-export function filterMarvelsWithoutThumbnail(characters) {
-    let marvelWithThumbnail = [];
-    const limit = Math.min(characters.length, 100);
-    for (let i = 0; i < limit; i++) {
-        let name = characters[i].name;
-        let thumbnailOfMarvel = characters[i].thumbnail.path;
-        let validMarvel = doesNotContainSubstring(thumbnailOfMarvel, "image_not_available");
-        if (validMarvel) {
-
-            const marvel = {
-                id: characters[i].id,
-                thumbnail: characters[i].thumbnail.path,
-                thumbnailExtention: characters[i].thumbnail.extension,
-                description: characters[i].description,
-                name: characters[i].name
-            };
-            marvelWithThumbnail.push(marvel);
-        }
-    }
-    return marvelWithThumbnail;
-}
