e48f41a4582ba42499f92c14aa63eb521db426a6
lashksal <salman.lashkarara@diconium.com>
2025-09-16 11:06:56 +0200

    Update header

--- react-marvel/src/components/header/Header.js
+++ react-marvel/src/components/header/Header.js
@@ -5,11 +5,28 @@ import "./Header.css";
 const Header = () => {
   return (
       <header className="app-header">
-        <nav className="nav-links">
-          <Link to="/">Home</Link>
-          <span className="separator">|</span>
-          <Link to="/about">About</Link>
-        </nav>
+        <div className="header-content">
+          {/* Left: Logo / Title */}
+          <div className="logo">
+            <Link to="/">Marvel Land</Link>
+          </div>
+
+          {/* Middle: Nav Links */}
+          <nav className="nav-links">
+            <Link to="/">Home</Link>
+            <Link to="/about">About</Link>
+          </nav>
+
+          {/* Right: Search */}
+          <div className="search-container">
+            <input
+                type="text"
+                placeholder="Search Marvels..."
+                className="search-box"
+            />
+            <button className="search-btn">Search</button>
+          </div>
+        </div>
       </header>
   );
 };
