1b952f2581f24f33db7626788ee6400fc3efbb3a
lashksal <salman.lashkarara@diconium.com>
2025-09-20 14:08:25 +0200

    Restructure project

--- react-marvel/src/services/endpointsPath.js
+++
@@ -1,9 +0,0 @@
-// endpointsPath.js
-const path = () => {
-  const getCharacterById = 'v1/public/characters/{characterId}';
-  const getCharacters = 'v1/public/characters';
-
-  return {getCharacterById, getCharacters};
-}
-
-export default path;
